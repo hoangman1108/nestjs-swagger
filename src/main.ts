305eaacbc57715d63beb0eed6ca0c12d8c2f5867
hoang man <hoangman772@gmail.com>
2020-11-04 14:26:27 +0700

    cors, helmet

--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,24 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import * as config from 'config';
 import { Logger } from '@nestjs/common';
+import * as helmet from 'helmet';
+import * as config from 'config';
+import * as csurf from 'csurf';
+import * as rateLimit from 'express-rate-limit';
 
 async function bootstrap() {
   const serverConfig = config.get('server');
   const logger = new Logger('bootstrap');
-  const app = await NestFactory.create(AppModule);
-
-  if(process.env.NODE_ENV === 'development'){
-    app.enableCors();
-  } else{
-    // app.enableCors({
-    //   origin: 
-    // })
-  }
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.use(helmet());
+  app.use(csurf());
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 100, // limit each IP to 100 requests per windowMs
+    }),
+  );
 
   const options = new DocumentBuilder()
     .setTitle('Rest API CRUD')
@@ -30,7 +33,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   // app.useGlobalPipes(new ValidationPipe());
   SwaggerModule.setup('api/docs', app, document);
-  
+
   const port = process.env.PORT || serverConfig.port;
   await app.listen(port);
   logger.log(`Application listening on port ${port}`);
